a1ac5c1a2c2404e445c046b1dff272f703d42b47
Diego Bracellos <diego.bracellos@PROWAY.TREINA>
2025-05-20 11:50:34 -0300

    commit inicial do app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Text(
+      "Bem vindo ao Flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.lime, fontSize: 30.0),
+    ),
+  ));
+}
+
+
